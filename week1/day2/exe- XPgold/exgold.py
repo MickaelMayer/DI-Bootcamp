5d5195f268b646ce932e5881488e7d42f7cb5dd8
MickaelMayer <mayer.mickaelm@gmail.com>
2024-11-12 12:28:32 +0200

    exgold

---
+++ week1/day2/exe- XPgold/exgold.py
@@ -0,0 +1,25 @@
+#Print the following output in one line of code:
+
+print(("Hello world\n"*4) + ("I love python\n"*4))
+
+# ------------------------------------------
+
+# Ask the user to input a month (1 to 12).
+# Display the season of the month received :
+# Spring runs from March (3) to May (5)
+# Summer runs from June (6) to August (8)
+# Autumn runs from September (9) to November (11)
+# Winter runs from December (12) to February (2)
+
+month =  int (input("enter a month by his number from 1 to 12"))
+
+
+
+if 3 <= month <= 5: 
+    print("spring")
+elif 6 <= month <= 8:
+    print("summer")
+elif 9 <= month <= 11:
+    print("autumn")
+elif month == 12 or 1 <= month <= 2:
+    print("winter")
