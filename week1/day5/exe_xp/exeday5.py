aff4a805e53ca45ba73b202e4b94a9464d8dfc09
MickaelMayer <mayer.mickaelm@gmail.com>
2024-11-15 10:53:47 +0200

    exe1

---
+++ week1/day5/exe_xp/exeday5.py
@@ -0,0 +1,11 @@
+# Convert the two following lists, into dictionaries.
+# Hint: Use the zip method
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+keys_values = dict(zip(keys, values))
+
+print(keys_values)
+
+# ==========================================
