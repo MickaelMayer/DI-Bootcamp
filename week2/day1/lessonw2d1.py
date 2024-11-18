a6dd07b0bc929bd591f843c00fc21addaf88a334
MickaelMayer <mayer.mickaelm@gmail.com>
2024-11-18 10:31:12 +0200

    1st function

--- week2/day1/lessonw2d1.py
+++ week2/day1/lessonw2d1.py
@@ -1,5 +1,29 @@
-def say_hello(usurnam, ):
-    print("hello " + usurnam)
+# def say_hello(usurname, ):
+#     print("hello " + usurname)
     
+# say_hello("Mickael")
+
+# def say_hello(usurname, language):
+#     if language == "EN":
+#         print("Hello " + usurname)
+#     elif language == "FR":
+#         print("Bonjour " + usurname)
+#     else:
+#         print("we do not support this language")
+        
+# say_hello("Mickael", "EN")
+# say_hello("Mickael", "FR")
+# say_hello("Mickael","bel")
+# say_hello(language="FR", usurname="Menahem")
+# say_hello("Mickael",language="FR")
+
+
+def say_hello(usurname, language="EN"):
+    if language == "EN":
+        print("Hello " + usurname)
+    elif language == "FR":
+        print("Bonjour " + usurname)
+    else:
+        print("we do not support this language")
+        
 say_hello("Mickael")
-    
