16a1991d7812a1d34e7a267c02d9d3b210c8ebc6
MickaelMayer <mayer.mickaelm@gmail.com>
2024-12-28 23:43:18 +0200

    234exe

--- week5/day4/script.js
+++ week5/day4/script.js
@@ -51,16 +51,38 @@ for (const person of people){
 
 // 🌟 Exercise 2 : Your favorite colors
 // Instructions
-// Create an array called colors where the value is a list of your five favorite colors.
-// Loop through the array and as you loop console.log a string like so: “My #1 choice is blue”, “My #2 choice is red” ect… .
+// Create an array called colors where the value is a list of your five favorite color
+const colors = ["blue","red","yellow","green","violet"]
+
+
+//  Loop through the array and as you loop console.log a string like so: “My #1 choice is blue”, “My #2 choice is red” ect… .
 // Bonus: Change it to console.log “My 1st choice”, “My 2nd choice”, “My 3rd choice”, picking the correct suffix for each number.
 // Hint : create an array of suffixes to do the Bonus
+const suffixes = ["st", "nd", "rd"]; 
+for (let i = 0; i < colors.length; i++) {
+  const position = i + 1; 
+  let suffix = "th"; 
+  
+  if (position <= 3) {
+    suffix = suffixes[position - 1];
+  }
 
+  console.log(`My ${position}${suffix} choice is ${colors[i]}`);
+}
 
 // 🌟 Exercise 3 : Repeat the question
 // Instructions
 // Prompt the user for a number.
 // Hint : Check the data type you receive from the prompt (ie. Use the typeof method)
+
+
+
+// let userNumber
+// do{
+//     userNumber = prompt("enter a number")
+// }while(Number(userNumber)<10)
+
+
 
 // While the number is smaller than 10 continue asking the user for a new number.
 // Tip : Which while loop is more relevant for this situation?
@@ -68,33 +90,49 @@ for (const person of people){
 
 // 🌟 Exercise 4 : Building Management
 // Instructions:
-// const building = {
-//     numberOfFloors: 4,
-//     numberOfAptByFloor: {
-//         firstFloor: 3,
-//         secondFloor: 4,
-//         thirdFloor: 9,
-//         fourthFloor: 2,
-//     },
-//     nameOfTenants: ["Sarah", "Dan", "David"],
-//     numberOfRoomsAndRent:  {
-//         sarah: [3, 990],
-//         dan:  [4, 1000],
-//         david: [1, 500],
-//     },
-// }
-
-
-// Review about objects
-// Copy and paste the above object to your Javascript file.
-
+const building = {
+    numberOfFloors: 4,
+    numberOfAptByFloor: {
+        firstFloor: 3,
+        secondFloor: 4,
+        thirdFloor: 9,
+        fourthFloor: 2,
+    },
+    nameOfTenants: ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        sarah: [3, 990],
+        dan:  [4, 1000],
+        david: [1, 500],
+    },
+}
 // Console.log the number of floors in the building.
+console.log(building.numberOfFloors);
 
 // Console.log how many apartments are on the floors 1 and 3.
+console.log(building.numberOfAptByFloor.firstFloor);
+console.log(building.numberOfAptByFloor.thirdFloor);
 
 // Console.log the name of the second tenant and the number of rooms he has in his apartment.
+console.log(building.nameOfTenants[1]);
+console.log(building.numberOfRoomsAndRent.dan);
+
+
 
 // Check if the sum of Sarah’s and David’s rent is bigger than Dan’s rent. If it is, than increase Dan’s rent to 1200.
+const sarahRent = building.numberOfRoomsAndRent.sarah[1];
+const davidRent = building.numberOfRoomsAndRent.david[1];
+const danRent = building.numberOfRoomsAndRent.dan[1];
+
+const totalRent = sarahRent + danRent;
+
+if (totalRent > danRent){
+    building.numberOfRoomsAndRent.dan[1]= 1200
+    console.log(building.numberOfRoomsAndRent.dan[1]);
+    
+}else{
+    console.log("no need to update");
+
+}
 
 
 // 🌟 Exercise 5 : Family
