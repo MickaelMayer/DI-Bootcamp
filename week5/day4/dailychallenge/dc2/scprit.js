d07d2d1af5b9fe1ba4a500375ba3d08cffcb6db8
MickaelMayer <mayer.mickaelm@gmail.com>
2024-12-29 13:19:44 +0200

    dc2

---
+++ week5/day4/dailychallenge/dc2/scprit.js
@@ -0,0 +1,27 @@
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
+// Do this Daily Challenge by yourself, without looking at the answers on the internet.
+// *  
+// * *  
+// * * *  
+// * * * *  
+// * * * * *
+// * * * * * *
+
+let star = "*"
+let count = 1
+
+while (count < 6 ){
+    console.log(star);
+    star += "*"
+    count++
+}
+
+for (let i = 1; i <6; i++){
+    let star = ""
+    for (let a = 1; a <= i; a++){
+        star+= "*"
+    }
+    console.log(star);
+    
+}
