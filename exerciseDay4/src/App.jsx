73dd04bf1c473a43cb682cbfdfc93e28da9af1f7
MickaelMayer <mayer.mickaelm@gmail.com>
2025-03-06 20:53:45 +0200

    exe2

--- exerciseDay4/src/App.jsx
+++ exerciseDay4/src/App.jsx
@@ -4,15 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Car from './components/Car'
 import Garage from './components/Garage'
+import Events from './components/Events'
 
 function App() {
-  const carinfo = {name: "Ford", model: "Mustang", color: "red"};
+  // const carinfo = {name: "Ford", model: "Mustang", color: "red"};
 
   return (
     <>
     <div>
-     <Car carInfo={carinfo} />
-      <Garage size="small" />
+     {/* <Car carInfo={carinfo} />
+      <Garage size="small" /> */}
+      <h1>Welcom to the Events page</h1>
+      <Events/>
      </div>
     </>
   )
